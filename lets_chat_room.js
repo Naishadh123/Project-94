bd896ae8bc0c5b6b3f53fd795c0e2bd21caf89a7
Naishadh123 <84559186+Naishadh123@users.noreply.github.com>
2022-01-22 14:49:43 +0530

    Add files via upload

---
+++ lets_chat_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCI8GHeQX0v_9izP7xSTf0_dPE86U4ehGQ",
+    authDomain: "let-s-chat-app-27635.firebaseapp.com",
+    databaseURL: "https://let-s-chat-app-27635-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-app-27635",
+    storageBucket: "let-s-chat-app-27635.appspot.com",
+    messagingSenderId: "262966928563",
+    appId: "1:262966928563:web:9c6d6cf8a7b00437843cc6"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
